e350ef68dc168657b2515927039a3ad9e4c632af
rtritto <ariele.1994@gmail.com>
2024-12-07 17:44:27 +0100

    Lint

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import vikeSolid from 'vike-solid/vite'
-import { plugin as vike } from 'vike/plugin'
-import type { UserConfig } from 'vite'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
 import path from 'node:path'
+import autoprefixer from 'autoprefixer'
+import tailwindcss from 'tailwindcss'
+import { plugin as vike } from 'vike/plugin'
+import vikeSolid from 'vike-solid/vite'
+import type { UserConfig } from 'vite'
 
 export default {
   root: 'src',
